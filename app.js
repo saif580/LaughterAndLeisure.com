82417baf8df77a2618825d1cee79a6eded07c606
iamsaif <saifulhasan580@gmail.com>
2021-01-14 04:11:46 +0530

    fixes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewsRoutes=require('./routes/reviews');
 
 const mongoDBStore=require("connect-mongo")(session);
 
-const dbUrl='mongodb://localhost:27017/laughterandleisure';
+const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/laughterandleisure';
 
 mongoose.connect(dbUrl,{
     useNewUrlParser:true,
